6a5944fac4432ca4dc2eb5f6650966a3c189d73d
dragonDScript <agomila96@gmail.com>
2023-01-31 20:55:18 +0100

    Remove bulk

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import vscode from 'vscode';
-
-import { ExtensionHostStarter } from 'vs/platform/extensions/electron-main/extensionHostStarter'
-
-var extHost = new ExtensionHostStarter(null, null, null, null);
-extHost.createExtensionHost(false);
